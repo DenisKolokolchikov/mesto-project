d16e858a73bc8873003e7d7068d65bf8b93441dd
Artem <artem.aisin@yandex.ru>
2022-11-02 21:33:58 +0300

    fix: изменения в Section.js

--- src/components/section.js
+++ src/components/section.js
@@ -4,6 +4,10 @@ export class Section {
         this._container = containerSelector;    
     }
 
+    renderItem(data) { //получаем данные с сервера и используем их в renderItems
+        this._renderer(data);
+    }
+
     renderItems(items) {
         items.forEach(item => this.renderItem(item));
     }
