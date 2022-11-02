7f85710f0c8bf50f210cf458a9bb3bea3ed9340d
Artem <artem.aisin@yandex.ru>
2022-11-02 21:31:30 +0300

    fix: изменения в popup.js

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,31 +1,16 @@
 export class Popup {
-  constructor(popupSelector) {
-      this._popup = document.querySelector(popupSelector);
-  }
+    constructor(popupSelector) {
+        this.popupSelector = popupSelector;
+        this._handleEscClose = this._handleEscClose.bind(this);
+    }
+    
+    open(){
+        this.popupSelector.classList.add('popup_opened');
+        document.addEventListener('keydown', this._handleEscClose);
+    }
 
-  _hadldeEscUp = (evt) => {
-      if(evt.key === 'Escape'){
-          this.close();
-      }
-  }
-
-  open(){
-      this._popup.classList.add('popup_opened');
-      document.addEventListener('keyup', this._hadldeEscUp)
-  }
-
-  close(){
-      this._popup.classList.remove('popup_opened');
-      document.removeEventListener('keyup', this._hadldeEscUp)
-  }
-
-  setEventListener(){
-      console.log(this);
-      this._popup.addEventListener('mousedown', (evt) => {
-        if ((evt.target.classList.contains('popup_opened')) || (evt.target.classList.contains('button__close'))) {
-            this.close();
-        }
-      })
-  }
-
+    close(){
+        this.popupSelector.classList.remove('popup_opened');
+        document.removeEventListener('keydown', this._handleEscClose);
+    }
 }
