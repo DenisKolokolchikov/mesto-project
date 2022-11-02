c5cb20f7f4d15059dbb20e3a89bc146b72b161bb
Denis-Kolokolchikov <kolokolchikovdenis@yandex.ru>
2022-11-02 21:27:43 +0300

    fix FromValidator End

--- src/components/FormValidator.js
+++ src/components/FormValidator.js
@@ -29,5 +29,50 @@ export class FormValidator {
         this._hideError(formElement, inputElement);
         }
     };
+
+     //проверяем на валидность
+     _checkInputValidity(inputList) {
+        return inputList.every((inputElement) => {
+            return inputElement.validity.valid;
+        })  
+    }
+
+    //блокируем/разблокируем кнопку сохранить
+    toggleButtonState(inputList, button) {
+        if(this._checkInputValidity(inputList)) {
+            button.classList.remove(this._inactiveButtonClass);
+            button.disabled = false;
+        } else {
+            button.classList.add(this._inactiveButtonClass);
+            button.disabled = true;
+        } 
+    }
+
+    //очищаем поля
+    clearError(element) {
+        element.querySelectorAll('.error').forEach((span) => {
+            span.textContent = '';
+        })
+        element.querySelectorAll('.popup__input').forEach((input) => {
+            input.classList.remove('popup__input_state_invalid');
+          });
+    }
+
+    _setEventListener(formElement) {
+        const inputList = Array.from(formElement.querySelectorAll(this._inputSelector));
+        const submitButton = formElement.querySelector(this._submitButtonSelector);
+        this.toggleButtonState(inputList, submitButton); 
+
+        inputList.forEach(input => {
+            input.addEventListener('input', () => {
+                this._isInputValid(formElement, input)
+                this.toggleButtonState(inputList, submitButton);           
+            });
+        });
+    }
+
+    enableValidation() {
+        this._setEventListener(this._element);
+    }
 }
 
