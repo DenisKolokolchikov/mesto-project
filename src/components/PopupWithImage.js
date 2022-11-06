974d4d7dffbcf3220ebb71f832dc9ef39dcae264
Denis-Kolokolchikov <kolokolchikovdenis@yandex.ru>
2022-11-06 20:36:11 +0300

    исправление ошибок PopupWithImage

--- src/components/PopupWithImage.js
+++ src/components/PopupWithImage.js
@@ -3,11 +3,11 @@ import { Popup } from "./Popup";
 export class PopupWithImage extends Popup {
     constructor(popupElement) {
         super(popupElement);
+        this.imgElement = document.querySelector(".popup__big-image");
+        this.imgCaption = document.querySelector(".popup__big-title");
         }
 
         open(data) { 
-            this.imgElement = document.querySelector(".popup__big-image");
-            this.imgCaption = document.querySelector(".popup__big-title");
             this.imgElement.src = data.link;
             this.imgElement.alt = data.name;
             this.imgCaption.textContent = data.name;
